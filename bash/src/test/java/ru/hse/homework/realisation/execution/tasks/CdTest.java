d4a9ba08a1971d79d6416f0317c62f5b1d0ae34b
murfel <murnatty@gmail.com>
2019-02-23 17:40:58 +0300

    improve cd tests

--- bash/src/test/java/ru/hse/homework/realisation/execution/tasks/CdTest.java
+++ bash/src/test/java/ru/hse/homework/realisation/execution/tasks/CdTest.java
@@ -1,6 +1,8 @@
 package ru.hse.homework.realisation.execution.tasks;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import ru.hse.homework.realisation.Environment;
 
 import java.io.File;
@@ -8,16 +10,22 @@ import java.io.File;
 import static org.junit.Assert.assertEquals;
 
 public class CdTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void cdSomewhere() throws Exception {
         Environment environment = new Environment();
-        String before = environment.getCurrentPath().toString();
+        environment.setCurrentPath(folder.getRoot().toPath());
 
-        Ls lsTask = new Ls();
-        lsTask.setArgs(new String[]{"src"});
-        String result = lsTask.execute(environment);
+        File innerFolder = folder.newFolder("innerFolder");
+
+        Cd cdTask = new Cd();
+        cdTask.setArgs(new String[]{"innerFolder"});
+        String result = cdTask.execute(environment);
 
         assertEquals("", result);
-        assertEquals(before + File.separator + "src", environment.getCurrentPath().toString());
+        assertEquals(innerFolder.getAbsolutePath(),
+                environment.getCurrentPath().toAbsolutePath().toString());
     }
 }
